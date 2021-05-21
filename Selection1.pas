8d01a79a023966fba0cd213353a7da870aed3d67
A G Kasun Manohara <84174391+agkasun@users.noreply.github.com>
2021-05-21 16:50:32 +0530

    Create Selection1.pas

---
+++ Selection1.pas
@@ -0,0 +1,12 @@
+program Selection1;
+
+var x:integer;
+begin
+    write('Enter your number :');
+    readln(x);
+    If x > 500 Then
+          x := x - 500;
+  
+    writeln('Output value is ', x);
+    readln;
+end.
