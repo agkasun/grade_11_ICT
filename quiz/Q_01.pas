a6ffe1367a1764b358168944e8386fc21e19cd32
A G Kasun Manohara <84174391+agkasun@users.noreply.github.com>
2021-05-24 14:37:33 +0530

    Add files via upload

---
+++ quiz/Q_01.pas
@@ -0,0 +1,13 @@
+program Q_01;
+
+var N1,N2,Value:Integer;
+
+Begin
+write('Insert First Number : ');
+readln(N1);
+write('Insert Second Number : ');
+readln(N2);
+Value := N1 * N2;
+writeln('Calculation Value is ',Value);
+readln;
+End.
---
+++ quiz/Q_02.pas
@@ -0,0 +1,17 @@
+program Q_01;
+
+var B,P:Integer;
+var BookPrice,PenPrice,Amount:Real;
+
+Begin
+BookPrice := 150.00;
+PenPrice := 10.00;
+
+write('Insert Number of Books : ');
+readln(B);
+write('Insert Number of  Pens : ');
+readln(P);
+Amount :=  (B * BookPrice) + (P * PenPrice);
+writeln('Bill Amount is ',Amount:0:2);
+readln;
+End.
